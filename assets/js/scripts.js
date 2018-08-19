789ccdddb88a34e3641ee86c480aebe2be1bc7a0
nancyhenry89 <nancyhenry89@gmail.com>
2018-08-19 16:19:27 +0200

    Functionality fixes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,7 +15,17 @@ $(document).ready(function() {
         $('.selected-country img').attr("src", "assets/imgs/" + img + ".png");
         var title=$(this).find('.title').text();
         $('.selected-country .title').text(title);
-
+        $('.bg').css('background','url(assets/imgs/'+img+'bg.png)');
+        if(img=="mal"){
+            $('.desc .title').text('The ultimate beach vacation!');
+            $('.desc p').text('There is seriously no better spot for eather a romantic vacation or family trip');
+        }else         if(img=="val"){
+            $('.desc .title').text('The ultimate Ski Resort!');
+            $('.desc p').text('Val Thorens takes the prize as the highest ski resort in Europe');
+        }else         if(img=="las"){
+            $('.desc .title').text('The Perfect Las Vegas Vacation');
+            $('.desc p').text('Luxor hotel and casino, romantic city or just a nice family visit!');
+        }
 
     });
     $('.choose').click(function() {
